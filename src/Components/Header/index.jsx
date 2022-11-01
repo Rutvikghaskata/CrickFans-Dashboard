20b031a1d397594aafcd8d1c78d43bf7a352bade
rutvikghaskata123 <rutik.dvij@gmail.com>
2022-11-01 12:36:45 +0530

    Worked on Responsive section for mobile view

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.scss";
-function index() {
+import { CgMenuRight } from "react-icons/cg";
+import { Link, useLocation } from "react-router-dom";
+
+function Header() {
+  const [menu, setMenu] = useState(false);
+  let location = useLocation();
+  const CURRENT_WB_NAME = location.pathname.split("/")[1];
   return (
-    <div className="header-wrapper">
-      <span>Dashboard</span>
+    <div className={menu ? "header-wrapper menu" : "header-wrapper"}>
+      <div className="top-sec">
+        <span>Dashboard</span>
+        <div
+          className="menu-section"
+          onClick={() => {
+            setMenu(!menu);
+          }}
+        >
+          <CgMenuRight className="icon" />
+        </div>
+      </div>
+      <ul>
+        <li
+          className={CURRENT_WB_NAME === "dashboard" && "active"}
+          onClick={() => {
+            setMenu(false);
+          }}
+        >
+          <Link to={"./dashboard"} className="list-item">
+            Dashboard
+          </Link>
+        </li>
+        <li
+          className={CURRENT_WB_NAME === "videos" && "active"}
+          onClick={() => {
+            setMenu(false);
+          }}
+        >
+          <Link to={"./videos"} className="list-item">
+            Videos
+          </Link>
+        </li>
+        <li
+          className={CURRENT_WB_NAME === "photos" && "active"}
+          onClick={() => {
+            setMenu(false);
+          }}
+        >
+          <Link to={"./photos"} className="list-item">
+            Photos
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
 
-export default index;
+export default Header;
